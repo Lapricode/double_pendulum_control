681aa691a0615db3c999f40909fe6064c7ea1062
Lapricode <prilampros@gmail.com>
2025-10-06 14:21:26 +0300

    small changes

--- pendu_move.py
+++ pendu_move.py
@@ -25,24 +25,31 @@ for id in motor_ids:
 
 t = 0.0
 dt = 0.01
+total_time = 16
 
 # begin auto update loop (it starts in the background)
 # it periodically updates the data in candle.md80s vector
 candle.begin()
-
 motor1 = candle.md80s[1]
 motor2 = candle.md80s[0]
 motor1.setImpedanceControllerParams(0.2, 0.02)
 motor2.setImpedanceControllerParams(0.2, 0.02)
-for i in range(1000):
-    motor1.setTargetPosition(math.pi * math.sin(t))
-    motor2.setTargetPosition(math.pi * math.sin(t))
-    # motor1.setTargetPosition(math.pi)
-    # motor2.setTargetPosition(0.0)
-    # motor1.setTargetPosition(math.pi)
-    # motor2.setTargetPosition(math.pi)
-    # motor1.setTargetPosition(0.0)
-    # motor2.setTargetPosition(math.pi)
+
+for i in range(int(total_time / dt)):
+    # motor1.setTargetPosition(math.pi * math.sin(t))
+    # motor2.setTargetPosition(math.pi * math.sin(t))
+    if t <= total_time / 4.:
+        motor1.setTargetPosition(0.0)
+        motor2.setTargetPosition(0.0)
+    elif t <= 2. * total_time / 4.:
+        motor1.setTargetPosition(0.0)
+        motor2.setTargetPosition(math.pi)
+    elif t <= 3. * total_time / 4.:
+        motor1.setTargetPosition(math.pi)
+        motor2.setTargetPosition(math.pi)
+    elif t <= 4. * total_time / 4.:
+        motor1.setTargetPosition(math.pi)
+        motor2.setTargetPosition(0.0)
     t += dt
     time.sleep(dt)
     
